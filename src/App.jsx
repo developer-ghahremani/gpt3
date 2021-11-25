b0e6a9d9a7f6ee3e3ba4cd5443c9961380f85cd2
Reza Ghahremani <developer.ghahremani@gmail.com>
2021-11-25 13:26:49 +0330

    firstly init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
---
+++ src/container/Feature/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./feature";
+const Feature = () => {
+  return <div>Feature</div>;
+};
+
+export default Feature;
